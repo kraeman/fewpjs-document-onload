7c3cdc62e470ca109f09e7a5057415926f074bd5
Krae <kraewind@gmail.com>
2021-05-02 18:29:15 -0400

    commit tree 4d82579e5f8524506c98f8888ac757af6412ad1b

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let thing = document.querySelector('#text')
+    thing.innerHTML = "This is really cool!"
+  }); 
